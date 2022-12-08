7dc211f4bfe5a276589877d2363c7b0f575eb68c
srinu-developer <srinivas77m99@gmail.com>
2022-12-08 08:39:55 +0530

    automation

--- app2.js
+++ app2.js
@@ -5,43 +5,35 @@ const expect = chai.expect;
 const MyClass = require('./test2.js');
 const myObj = new MyClass();
 
-describe("Automation for few operations", ()=>{
-
-    before(function(){
-        console.log("-------- Starts Here -----------")
-    })
-
-    after(function(){
-        console.log("--------- Ends Here -----------")
-    })
+describe("Automation TestCases", ()=>{
 
     it("Test case for Addition", ()=>{
-        expect(myObj.add(6,6)).to.be.equal(12);
+        expect(myObj.add(2,2)).to.be.equal(4);
     })
 
-    it("Test case for Subtraction", ()=>{
-        expect(myObj.sub(10,8)).to.be.equal(2);
+    it("Test case fot Subtraction", ()=>{
+        expect(myObj.sub(4,2)).to.be.equal(2);
     })
 
-    it("Test case for Multiplication", ()=>{
-        expect(myObj.multi(3,3)).to.be.equal(9);
+    it("Test case for Multiplication",()=>{
+        expect(myObj.multi(2,2)).to.be.equal(4);
     })
 
-    it("Test case for Division", ()=>{
-        expect(myObj.div(10,5)).to.be.equal(2);
+    it("Test case for Divison", ()=>{
+        expect(myObj.div(4,2)).to.be.equal(2);
     })
 
-    it("Test case for SPY", ()=>{
-        var data = sinon.spy(myObj,"add");
-        var arg1 =10, arg2=10;
+    it("Test case for Sinon", ()=>{
+        let spy = new sinon.spy(myObj,"add");
+        let arg1=10,arg2=20;
         myObj.callAnotherFn(arg1,arg2);
-        sinon.assert.calledOnce(data);
-        expect(data.calledOnce).to.be.true;
+        sinon.assert.calledOnce(spy);
+        expect(spy.calledOnce).to.be.true;
     })
 
-    it("Test case for Sinon",()=>{
-        var callback = sinon.spy();
+    it("Test case for spy", ()=>{
+        let callback = new sinon.spy();
         myObj.call(callback);
         expect(callback.calledOnce).to.be.true;
     })
-})
+})    
--- test2.js
+++ test2.js
@@ -1,34 +1,31 @@
-class MyClass{
+class MyClass {
     constructor(){
-        console.log("Automation starts Here");
+        console.log("Automation Starts Here");
     }
 
     add(arg1,arg2){
-        var result;
+        let result;
         result = arg1 + arg2;
         return result;
     }
 
     sub(arg1,arg2){
-        var result;
-        result = arg1 - arg2;
+        let result = arg1 - arg2;
         return result;
     }
 
     multi(arg1,arg2){
-        var result;
-        result = arg1 * arg2;
+        let result = arg1 * arg2;
         return result;
     }
 
     div(arg1,arg2){
-        var result;
-        result = arg1 / arg2;
+        let result = arg1 / arg2;
         return result;
     }
 
     callAnotherFn(arg1,arg2){
-        var result = this.add(arg1,arg2);
+        let result = this.add(arg1,arg2);
         return result;
     }
 
